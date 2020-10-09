3e11a61da0e0f49800f6f669314a5177ce1d0eca
Adam Woodhead <awoodhead@bconway.co.uk>
2020-10-09 14:17:02 +0100

    unix timestamp converter

--- Data Connection/Helper.cs
+++ Data Connection/Helper.cs
@@ -35,5 +35,13 @@ namespace DataConnection
         {
             return !string.IsNullOrEmpty(dataPacketList.FirstOrDefault().Error) || dataPacketList.FirstOrDefault().Errors != null;
         }
+
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
     }
 }
